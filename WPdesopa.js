601ecb4e740fb00a8e377cc2a0ce888fddab2703
Doug Luce <luce@inbound.aaronsen.com>
2012-01-23 15:04:35 -0800

    Finished code for GitHub

---
+++ WPdesopa.js
@@ -0,0 +1,5 @@
+window.setInterval(function() {
+  document.getElementById('content').style.display="inline";
+  document.getElementById('quickbar').style.display="inline";
+  document.getElementById('mw-sopaOverlay').style.display="none";
+ }, 2);
